258d1b963c58dcd8fa0b1630197019df9652d9c4
Corbin Moore <corbin.moore27@gmail.com>
2018-05-09 20:24:42 -0600

    readings api route update

--- routes/readingAPI.js
+++ routes/readingAPI.js
@@ -1,7 +1,13 @@
 var db = require("../models");
 
 module.exports = function(app) {
-
+  app.get("/api/Reading", function(req, res) {
+    // 1. Add a join to include all of each Reading's Posts
+    db.Reading.findAll({}).then(function(readings) {
+      res.json(readings);
+    });
+  });
+  
   app.get("/api/Reading/:mac", function(req, res) {
     // 2; Add a join to include all of the Reading's Posts here
     db.Reading.findAll({
